e1f88bf2ea07d7236755a530083ed85a7b7db7b2
FZZahir <112213682+FZZahir@users.noreply.github.com>
2023-01-26 12:27:07 -0800

    Updates

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css'
+
+
 
 const Logo = () => {
 	return(
-		<div className = 'ma4 mt0'>
-			<img href= '' src =''/>
+		<div className='ma4 mt0'>
+			<Tilt className="Tilt br2 shadow-1" perspective={750} glareEnable={true} glareColor="yellow" glareMaxOpacity={0.95} style={{ height: 150, width: 150 }} >
+ 				<div className="Tilt-inner pa3"></div>
+			</Tilt>
 		</div>
 		);
 }
--- src/container/App.js
+++ src/container/App.js
@@ -2,12 +2,16 @@ import React, {Component} from 'react';
 import './App.css';
 import Navigation from '../components/Navigation/Navigation.js';
 import Logo from '../components/Logo/Logo.js';
+import Tilt from 'react-parallax-tilt';
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Navigation/>
       <Logo/>
+      
      {/* 
       <ImageLinkForm/>
       <FaceRecognition/>*/}
