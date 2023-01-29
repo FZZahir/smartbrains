1056d3b55fcd585c0c208a352433f6101521aae3
FZZahir <112213682+FZZahir@users.noreply.github.com>
2023-01-28 20:20:43 -0800

    updates

--- src/container/App.js
+++ src/container/App.js
@@ -6,7 +6,11 @@ import Rank from '../components/Rank/Rank.js';
 import Logo from '../components/Logo/Logo.js';
 import Tilt from 'react-parallax-tilt';
 import ParticlesBg from 'particles-bg';
-
+/*const Clarifai = require('clarifai');*/
+import Clarifai from 'clarifai'; /*new way rather than old require way*/
+const app = new Clarifai.App({
+    apiKey: '2795a8038cf04b13b778c692f75fad67'
+});
 class App extends Component {
   constructor(){
     super();
@@ -18,8 +22,17 @@ class App extends Component {
   OnInputChange= (event) =>{
     console.log(event.target.value);
   }
-  onSubmit = () =>{
-    console.log("Click")
+  OnSubmit = () =>{
+    console.log("Click");
+    app.model.predict("45fb9a671625463fa646c3523a3087d5","https://sample.clarifai.com/face-det.jpg").then(
+      function(response){
+        //do something
+        console.log(response);
+      },
+      function(err){
+        //do something error
+      }
+    );
   }
   render() {
     return (
@@ -28,7 +41,7 @@ class App extends Component {
       <Navigation/>
       <Logo/>
       <Rank/>
-      <ImageLinkForm OnInputChange={this.OnInputChange}/>
+      <ImageLinkForm OnInputChange={this.OnInputChange} OnSubmit={this.OnSubmit}/>
       
      {/* 
       <FaceRecognition/>*/}
